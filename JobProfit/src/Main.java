4bd05f41397aa3c17f3205cb4973385e9e1b7b87
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2023-05-15 01:18:59 +0300

    remove space

--- JobProfit/src/Main.java
+++ JobProfit/src/Main.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int [][] arr={{1, 6, 6}, {2, 5, 5}, {5, 7, 5}, {6, 8, 3}};
-        jobSchedulingProfit(arr);
-        System.out.println(jobSchedulingProfit(arr));
+        System.out.println();
     }
     public static int jobSchedulingProfit(int [][] job){
 
@@ -17,23 +15,18 @@ public class Main {
         TreeMap<Integer, Integer> startTimeIndex = new TreeMap<Integer, Integer>();
         for(int i=job.length-1;i>=0;i--){
 
-            //We are looking for the next non over lappoing index in the array
+            //We are looking for the next non overlapping index in the array
             //in other words we need the next start time which is greater than or equal the current end time
             //we look for the lowest Key in startTimeIndex which is greater than or equal to the current end time
             //then we get the Value of this key
             //if no key is found then it will = the length of the job array
-            int nextnonoverlappingIndex=java.util.Optional.ofNullable(
-                startTimeIndex.ceilingEntry(job[i][1])).map(e->e.getValue()).orElse(job.length);
+            int nextNonOverlappingIndex=java.util.Optional.ofNullable(startTimeIndex.ceilingEntry(job[i][1])).map(e->e.getValue()).orElse(job.length);
             //we check which profit is greater,the previous profit or
             //the current profit with the profit of the next non overlapping
-
-
-            optimalProfit[i]=Math.max(optimalProfit[i+1],job[i][2]+optimalProfit[nextnonoverlappingIndex]);
-            //then we put the current start time in the starttimeindex to begin the for loop again with the lower index
+            optimalProfit[i]=Math.max(optimalProfit[i+1],job[i][2]+optimalProfit[nextNonOverlappingIndex]);
+            //then we put the current start time in the startimeindex to begin the for loop again with the lower index
             startTimeIndex.put(job[i][0],i);
         }
-
-
         return optimalProfit[0];
     }
 }
