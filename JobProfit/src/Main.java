8d21f38beb4ccae01901b7d95001ab30a0334e63
samer <sameradel789@gmail.com>
2023-05-15 00:45:49 +0300

    comments on for loop

--- JobProfit/src/Main.java
+++ JobProfit/src/Main.java
@@ -14,9 +14,17 @@ public class Main {
         // and store them in ascending order according to the value of key
         TreeMap<Integer, Integer> startTimeIndex = new TreeMap<Integer, Integer>();
         for(int i=job.length-1;i>=0;i--){
+            //We are looking for the next non over lappoing index in the array
+            //in other words we need the next start time which is greater than or equal the current end time
+            //we look for the lowest Key in startTimeIndex which is greater than or equal to the current end time
+            //then we get the Value of this key
+            //if no key is found then it will = the length of the job array
             int nextnonoverlappingIndex=java.util.Optional.ofNullable(
-                    startTimeIndex.ceilingEntry(job[i][1])).map(e->e.getValue()).orElse(job.length);
+                startTimeIndex.ceilingEntry(job[i][1])).map(e->e.getValue()).orElse(job.length);
+            //we check which profit is greater,the previous profit or
+            //the current profit with the profit of the next non overlapping
             optimalProfit[i]=Math.max(optimalProfit[i+1],job[i][2]+optimalProfit[nextnonoverlappingIndex]);
+            //then we put the current start time in the starttimeindex to begin the for loop again with the lower index
             startTimeIndex.put(job[i][0],i);
         }
 
