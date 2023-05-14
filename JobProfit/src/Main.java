4cad610fb97800209023c3b6f963ef05de8c3426
samer <sameradel789@gmail.com>
2023-05-14 22:28:09 +0300

    Adding the for loop

--- JobProfit/src/Main.java
+++ JobProfit/src/Main.java
@@ -13,6 +13,12 @@ public class Main {
         // TreeMap that will take start time (Key) and index of that starting time (value)
         // and store them in ascending order according to the value of key
         TreeMap<Integer, Integer> startTimeIndex = new TreeMap<Integer, Integer>();
+        for(int i=job.length-1;i>=0;i--){
+            int nextnonoverlappingIndex=java.util.Optional.ofNullable(
+                    startTimeIndex.ceilingEntry(job[i][1])).map(e->e.getValue()).orElse(job.length);
+            optimalProfit[i]=Math.max(optimalProfit[i+1],job[i][2]+optimalProfit[nextnonoverlappingIndex]);
+            startTimeIndex.put(job[i][0],i);
+        }
 
 
         return optimalProfit[0];
